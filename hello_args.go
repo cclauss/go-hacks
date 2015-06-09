e001de381b1263ab241593c846036bc256cacb50
cclauss <cclauss@bluewin.ch>
2015-06-09 17:59:29 +0200

    Shebang-like line for go

---
+++ hello_args.go
@@ -0,0 +1,19 @@
+///usr/bin/env go run "$0" "$@" ; exit "$?"
+// the line above is a shebang-like line for go
+// chmod +x hello_args.go
+// ./hello_args.go
+// echo "$?" # unfortunately it is 1 instead of 7 but at least it is not zero!
+
+package main
+
+import(
+    "fmt"
+    "os"
+    "strings"
+)
+
+func main() {
+    fmt.Println(strings.Join(os.Args[1:], " "))
+    fmt.Printf("Hello, world.\n")
+    os.Exit(7)
+}
