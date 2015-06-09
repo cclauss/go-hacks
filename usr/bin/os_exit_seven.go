2ba49e2fe54b75035662d6f5f8e40d9fb564817e
cclauss <cclauss@bluewin.ch>
2015-06-09 17:54:20 +0200

    Shebang-like line for go

---
+++ usr/bin/os_exit_seven.go
@@ -0,0 +1,13 @@
+///usr/bin/env go run "$0" "$@"; exit "$?"
+// a shebang-like line for go
+// chmod +x os_exit_seven.go
+// ./os_exit_seven.go
+// echo "$?"  # unfortunately it is 1 instead of 7 but at least it is not zero!
+
+package main
+
+import "os"
+
+func main() {
+    os.Exit(7)
+}
