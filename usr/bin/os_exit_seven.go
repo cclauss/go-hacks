41c1d4cfe2537278f6c2d43bba73380142563395
cclauss <cclauss@bluewin.ch>
2015-06-09 17:54:49 +0200

    Update os_exit_seven.go

--- usr/bin/os_exit_seven.go
+++ usr/bin/os_exit_seven.go
@@ -1,5 +1,5 @@
 ///usr/bin/env go run "$0" "$@"; exit "$?"
-// a shebang-like line for go
+// the line above is a shebang-like line for go
 // chmod +x os_exit_seven.go
 // ./os_exit_seven.go
 // echo "$?"  # unfortunately it is 1 instead of 7 but at least it is not zero!
