2a4c037ca4ea4805823bae970b7d620c290c93a5
cclauss <cclauss@bluewin.ch>
2017-04-10 23:02:32 +0200

    Update pypi_info.go

--- pypi_info.go
+++ pypi_info.go
@@ -6,21 +6,93 @@
 
 package main
 
-import "fmt"
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+)
 
-func funcName(input1 type1, input2 type2) (output1 type1, output2 type2) {
+// func funcName(input1 type1, input2 type2) (output1 type1, output2 type2) {
 
-func pypi_info(package_name string) (resp *Response, err error) {
-    url := 'https://pypi.python.org/pypi/' + package_name
-    fmt.Printf("%v\n", url)
-    return http.Get(url)
+func pypiHTML(packageName string) (string, error) {
+	url := "https://pypi.python.org/pypi/" + packageName
+	fmt.Printf("%v\n", url)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(bytes), err
+}
 
-// resp, err := http.Get("http://example.com/")
-//if err != nil {
-//	// handle error
-//}
+func pypiInfo(packageName string) (map[string]interface{}, error) {
+	url := "https://pypi.python.org/pypi/" + packageName + "/json"
+	// fmt.Printf("%v\n", url)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var jsonData interface{}
+	err = json.Unmarshal(bytes, &jsonData)
+	if err != nil {
+		log.Fatal(err)
+	}
+	jsonData = jsonData.(map[string]interface{})["info"]
+	return jsonData.(map[string]interface{}), err
+}
+
+func pypiRecord(ch chan string, packageName string) {
+	info, err := pypiInfo(packageName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	py2Only := false
+	py3Support := false
+	for _, classifier := range info["classifiers"].([]interface{}) {
+		s := classifier.(string)
+		// println(i, s)
+		if s == "Programming Language :: Python :: 2 :: Only" {
+			py2Only = true
+		} else if strings.HasPrefix(s, "Programming Language :: Python :: 3") {
+			py3Support = true
+		}
+	}
+	r := []string{
+		strconv.FormatBool(py2Only),
+		strconv.FormatBool(py3Support),
+		info["version"].(string),
+		info["package_url"].(string),
+	}
+	ch <- strings.Join(r, ",")
+	// return strings.Join(r, ","), err
+}
 
 func main() {
-	fmt.Printf("%v (%v)\n", pypi_info('requests'))
+	ch := make(chan string)
+	packageNames := []string{"requests", "aiohttp", "pip", "ansible", "supervisor", "newrelic_plugin_agent"}
+	for _, packageName := range packageNames {
+		//		s := strings.Join(pypiRecord(packageName), ",")
+		go pypiRecord(ch, packageName)
+		// if err != nil {
+		//	log.Fatal(err)
+		//}
+		//println(s)
+	}
+
+	for i := range packageNames {
+		println(i, <-ch)
+	}
 }
