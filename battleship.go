748bd23b93edf356338a87f0033872c8d1727ccb
cclauss <cclauss@bluewin.ch>
2017-04-27 22:04:26 +0200

    Update battleship.go

--- battleship.go
+++ battleship.go
@@ -1,6 +1,7 @@
 package main
 
 /*
+            H  u  m  a  n                  C  o  m  p  u  t  e  r
 +------------------------------------------------------------------------+
 |     A  B  C  D  E  F  G  H  I  J  ||  A  B  C  D  E  F  G  H  I  J     |
 |  1                                 1                                 1 |
@@ -21,29 +22,25 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
-
-const a = "  AAAAA   "
-const b = "   BBBB   "
-const c = "CCC       "
-const s = "       SSS"
-const d = "    DD    "
 
 const blanks = "          "
 const hitAndMiss = "!."
+const hitAndMissAndSpace = hitAndMiss + " "
 const letters = "ABCDEFGHIJ"
+const title = "            H  u  m  a  n                  C  o  m  p  u  t  e  r"
 
-var ships = func() map[string]int {
-	ships := make(map[string]int)
-	ships["Aircraft Carrier"] = 5
-	ships["Battleship"] = 4
-	ships["Cruiser"] = 3
-	ships["Submarine"] = 3
-	ships["Destroyer"] = 2
-	return ships
-}()
+var ships = map[string]int{
+	"Aircraft Carrier": 5,
+	"Battleship":       4,
+	"Cruiser":          3,
+	"Submarine":        3,
+	"Destroyer":        2,
+}
 
 type point struct {
 	X, Y int
@@ -54,21 +51,62 @@ type player struct {
 	board []string
 }
 
-func letterNumberToPoint(s string) (point, error) {
+func invalidPoint(pt point) bool {
+	return pt.X < 0 || pt.X > 9 || pt.Y < 0 || pt.Y > 9
+}
+
+func randomBool() bool { return rand.Intn(2) == 1 }
+
+func randomPoint() point { return point{rand.Intn(10), rand.Intn(10)} }
+
+func strReplaceRune(s string, pos int, r rune) string {
+	// replace the rune at index pos with rune r
+	return strings.Join([]string{s[:pos], s[pos+1:]}, string(r))
+}
+
+func pointsForShip(topLeft point, length int, across bool) (pts []point) {
+	for i := 0; i < length; i++ {
+		if across {
+			pts = append(pts, point{topLeft.X + i, topLeft.Y})
+		} else {
+			pts = append(pts, point{topLeft.X, topLeft.Y + i})
+		}
+	}
+	// if last point is invalid...
+	if invalidPoint(pts[len(pts)-1]) {
+		pts = []point{}
+	}
+	return
+}
+
+func pointToLetterNumber(pt point) (string, error) {
+	if invalidPoint(pt) {
+		return "", errors.New(fmt.Sprintf("invalid point %v.", pt))
+	}
+	return fmt.Sprintf("%s%d", letters[pt.X], pt.Y+1), nil
+}
+
+func letterNumberToPoint(s string) (pt point, err error) {
 	s = strings.ToUpper(s)
 	x := strings.Index(letters, s[:1])
 	y := strings.Index("12345678910", s[1:])
-	if x == -1 || y == -1 || y == 10 {
+	pt = point{x, y}
+	if invalidPoint(pt) {
 		if s[:1] == "Q" {
 			panic("User quit.")
 		}
-		return point{x, y}, errors.New("invalid: Try 'A1' or 'J10'")
+		err = errors.New("invalid: Try 'A1' or 'J10'")
 	}
-	return point{x, y}, nil
+	return
 }
 
 func borderRow() string {
 	return strings.Join([]string{"+", "+"}, strings.Repeat("-", 72))
+}
+
+func letterRow() string {
+	letters := strings.Join(charsInStr(letters), "  ")
+	return fmt.Sprintf("|     %s  ||  %s     |", letters, letters)
 }
 
 func charsInStr(str string) []string {
@@ -81,7 +119,6 @@ func charsInStr(str string) []string {
 }
 
 func clokeInStr(str string) []string {
-	// return [c for c in str]
 	letters := []string{}
 	for _, c := range str {
 		if strings.ContainsRune(hitAndMiss, c) == false {
@@ -92,64 +129,181 @@ func clokeInStr(str string) []string {
 	return letters
 }
 
-func letterRow() string {
-	letters := strings.Join(charsInStr(letters), "  ")
-	return fmt.Sprintf("|     %s  ||  %s     |", letters, letters)
-}
-
 func formatRow(i int) string {
 	return fmt.Sprintf("| %2d  %s  %#2[1]d  %s  %#2[1]d |", i, "%s")
 }
 
 func board(homeTeam, awayTeam player) string {
-	rows := []string{borderRow(), letterRow()}
+	rows := []string{title, borderRow(), letterRow()}
 	for i := 0; i < 10; i++ {
 		h := strings.Join(charsInStr(homeTeam.board[i]), "  ")
 		a := strings.Join(clokeInStr(awayTeam.board[i]), "  ")
 		rows = append(rows, fmt.Sprintf(formatRow(i+1), h, a))
 	}
-	return strings.Join(append(append(rows, rows[1]), rows[0]), "\n")
+	return strings.Join(append(append(rows, rows[2]), rows[1]), "\n")
 }
 
-func makeHumanPlayer() player {
-	return player{"human", []string{a, blanks, b, blanks, c, blanks, s,
-		blanks, d, blanks}}
-}
-func makeCompuPlayer() player {
-	return player{"computer", []string{blanks, a, blanks, b, blanks, c,
-		blanks, s, blanks, d}}
+func makePlayer(name string) player {
+	return player{name, []string{blanks, blanks, blanks, blanks, blanks,
+		blanks, blanks, blanks, blanks, blanks}}
 }
 
-func askWhichSquare(maxSquare string) string {
-	fmt.Printf("maxSquare: %s\n", maxSquare)
-	reader := bufio.NewReader(os.Stdin)
-	// for {
-	fmt.Print("Enter text: ")
-	text, _ := reader.ReadString('\n')
-	text = strings.TrimSpace(text)
-	fmt.Printf("%T|%s|\n", text, text)
-	fmt.Println(letterNumberToPoint(text))
-	return text
-	// fmt.Println(board(compuPlayer, humanPlayer))
-}
-
-func humanTurn() {}
-func compuTurn() {}
-
-func main() {
-	humanPlayer := makeHumanPlayer()
-	compuPlayer := makeCompuPlayer()
-	gameOn := true
-	for gameOn == true {
-		fmt.Println(board(compuPlayer, humanPlayer))
-		sq := askWhichSquare("J10")
-		if sq[:1] == "Q" {
-			gameOn = false
-		} else {
-
-			humanTurn()
-			compuTurn()
+func playableSquares(opponent player) []point {
+	squares := []point{}
+	for y, row := range opponent.board {
+		for x, c := range row {
+			if strings.ContainsRune(hitAndMiss, c) == false {
+				squares = append(squares, point{x, y})
+			}
 		}
 	}
-	fmt.Println(board(compuPlayer, humanPlayer))
+	fmt.Printf("%v\n", squares)
+	return squares
+}
+
+func hasShip(p player, r rune) bool {
+	return strings.ContainsRune(strings.Join(p.board, ""), r)
+}
+
+func hasAnyShips(p player) bool {
+	for _, c := range strings.Join(p.board, "") {
+		if strings.ContainsRune(hitAndMissAndSpace, c) == false {
+			return true
+		}
+	}
+	fmt.Printf("Game Over: Player %s has no ships!\n", p.name)
+	return false
+}
+
+func askUser(prompt string) (string, error) {
+	fmt.Print(prompt + " ")
+	text, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	return strings.TrimSpace(text), err
+}
+
+func askWhichSquare() string {
+	text, _ := askUser("Enter a square between A1 and J10:")
+	pt, _ := letterNumberToPoint(text)
+	// fmt.Printf("(%T) %s ==> %v|\n", text, text, pt)
+	return text
+}
+
+func askIfAcross() bool {
+	text, _ := askUser("[A]cross or [D]own:")
+	return strings.ToUpper(text)[0] == 'A'
+}
+
+func dropABomb(opponent player, sq point) (gameOn bool) {
+	gameOn = true
+	oldRune := rune(opponent.board[sq.Y][sq.X])
+	itsAHit := oldRune != ' '
+	splash := '.'
+	if itsAHit == true {
+		splash = '!'
+	}
+	// Drop the bomb into the row
+	opponent.board[sq.Y] = strReplaceRune(opponent.board[sq.Y], sq.X, splash)
+	if itsAHit {
+		if hasShip(opponent, rune(oldRune)) {
+			fmt.Println("It's a hit!")
+		} else {
+			fmt.Printf("You sunk my battleship! %c\n", oldRune)
+			gameOn = hasAnyShips(opponent)
+		}
+	}
+	return
+}
+
+func humanTurn(opponent player) (gameOn bool) {
+	sq := askWhichSquare()
+	if sq[:1] == "Q" {
+		gameOn = false // human wants out
+	} else {
+		pt, _ := letterNumberToPoint(sq)
+		oldRune := rune(opponent.board[pt.Y][pt.X])
+		if strings.ContainsRune(hitAndMiss, oldRune) {
+			fmt.Println("You already tried that square.  Try different one:")
+			gameOn = humanTurn(opponent)
+		} else {
+			gameOn = dropABomb(opponent, pt)
+		}
+	}
+	return
+}
+
+func compuTurn(opponent player) (gameOn bool) {
+	ps := playableSquares(opponent)
+	if len(ps) == 0 {
+		panic("No playable squares!!")
+	}
+	sq := ps[rand.Intn(len(ps))]
+	gameOn = dropABomb(opponent, sq)
+	return
+}
+
+func humanPlaceShip(p player, shipName string) {
+	letter := shipName[0]
+	length := ships[shipName]
+	fmt.Printf("Placing %s (%c * %d)...\n", shipName, letter, length)
+	topLeft, _ := letterNumberToPoint(askWhichSquare())
+	across := askIfAcross()
+	pts := pointsForShip(topLeft, length, across)
+	if len(pts) == 0 {
+		humanPlaceShip(p, shipName)
+		return
+	}
+	for _, pt := range pts {
+		oldRune := p.board[pt.Y][pt.X]
+		if oldRune != ' ' {
+			fmt.Printf("Placing %s failed: %v is %c\n", shipName, pt, oldRune)
+			humanPlaceShip(p, shipName)
+			return
+		}
+	}
+	for _, pt := range pts {
+		p.board[pt.Y] = strReplaceRune(p.board[pt.Y], pt.X, rune(letter))
+	}
+}
+
+func compuPlaceShip(p player, shipName string) {
+	letter := shipName[0]
+	length := ships[shipName]
+	topLeft := randomPoint()
+	across := randomBool()
+	pts := pointsForShip(topLeft, length, across)
+	if len(pts) == 0 {
+		compuPlaceShip(p, shipName)
+		return
+	}
+	for _, pt := range pts {
+		oldRune := p.board[pt.Y][pt.X]
+		if oldRune != ' ' {
+			// fmt.Printf("Placing %s failed: %v is %c\n", shipName, pt, oldRune)
+			compuPlaceShip(p, shipName)
+			return
+		}
+	}
+	for _, pt := range pts {
+		p.board[pt.Y] = strReplaceRune(p.board[pt.Y], pt.X, rune(letter))
+	}
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	humanPlayer := makePlayer("human")
+	compuPlayer := makePlayer("computer")
+	for shipName := range ships {
+		fmt.Println(board(humanPlayer, compuPlayer))
+		humanPlaceShip(humanPlayer, shipName)
+		compuPlaceShip(compuPlayer, shipName)
+	}
+	fmt.Println(board(humanPlayer, compuPlayer))
+	gameOn := hasAnyShips(humanPlayer) && hasAnyShips(compuPlayer)
+	for gameOn {
+		gameOn = humanTurn(compuPlayer)
+		if gameOn {
+			gameOn = compuTurn(humanPlayer)
+		}
+		fmt.Println(board(humanPlayer, compuPlayer))
+	}
 }
